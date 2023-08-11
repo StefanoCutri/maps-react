e95b3a60eb6e2bacd9a99ac3b3985589a5ac90a4
Stefano Cutri <stefanocutristecco@gmail.com>
2023-08-11 17:45:12 -0300

    PlacesReducer

--- src/context/places/PlacesReducer.ts
+++ src/context/places/PlacesReducer.ts
@@ -1,1 +1,24 @@
-export {}
+import { PlacesState } from "./PlacesProvider";
+
+type PlacesAction = {
+  type: "setUserLocation";
+  payload: [number, number];
+};
+
+export const placesReducer = (
+  state: PlacesState,
+  action: PlacesAction
+): PlacesState => {
+  switch (action.type) {
+    case "setUserLocation":
+      return {
+        ...state,
+        isLoading: false,
+        userLocation: action.payload,
+      };
+      break;
+
+    default:
+      return state;
+  }
+};
