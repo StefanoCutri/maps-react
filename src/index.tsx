bcec634f83e957398a88da29f7ecf44bd5d9af02
Stefano Cutri <stefanocutristecco@gmail.com>
2023-08-14 16:01:30 -0300

    mapbox installed and configured

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlZmFub2N1dHJpIiwiYSI6ImNsbGIwMnN5cTAyOGozanBodmJhaWh2ZnQifQ.72s-LT2FM59dwJthpotr4w';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
