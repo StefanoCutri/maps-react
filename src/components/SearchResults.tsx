a2021e9aa8acebe4cb12a4a7708db87ae3340436
Stefano Cutri <stefanocutristecco@gmail.com>
2023-08-16 19:29:45 -0300

    Fly to place clicked

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
-import { PlacesContext } from "../context";
+import { MapContext, PlacesContext } from "../context";
 import { LoadingPlaces } from "./";
+import { Feature } from "../interfaces/places";
 
 export const SearchResults = () => {
   const { places, isLoadingPlaces } = useContext(PlacesContext);
-
+  const { map } = useContext(MapContext);
   if (isLoadingPlaces) {
     return <LoadingPlaces />;
   }
@@ -13,10 +14,18 @@ export const SearchResults = () => {
     return <></>;
   }
 
+  const onPlaceClick = (place: Feature) => {
+    const [lng, lnt] = place.center;
+    map?.flyTo({
+      zoom: 14,
+      center: [lng, lnt],
+    });
+  };
+
   return (
     <ul className="list-group mt-3">
       {places.map((place) => (
-        <li key={place.id} className="list-group-item list-group-item-action">
+        <li key={place.id} className="list-group-item list-group-item-action" onClick={() => onPlaceClick(place)}>
           <h6>{place.text_es}</h6>
           <p
             className="text-muted"
